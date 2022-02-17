c1b00725fabab19b78dde488dcb0007471c7a50b
clara <10088152+claraj@users.noreply.github.com>
2022-02-17 16:17:39 -0600

    completed github app

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template, request  # NOT the same as requests 
+from github_api import get_github_user
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
     return render_template('index.html')
+
+
+@app.route('/get_user')
+def get_user_info():
+    print('Form data is', request.args)  # just for debugging 
+    username = request.args.get('username')
+    if username:
+        user_info, error = get_github_user(username)
+        if error: 
+            return render_template('error.html', error=error)
+        else:
+            return render_template('github.html', user_info=user_info)
+    else:
+        return render_template('error.html', error='Enter a username')
 
 
 if __name__ == '__main__':
