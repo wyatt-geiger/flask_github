f32298f3ecf257ea5c5a7aeea6cdacc7050183c0
wyatt.geiger <wyatt.geiger@go.minneapolis.edu>
2022-02-25 02:00:33 -0600

    Updated with lecture notes/examples

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request  # NOT the same as requests 
+from github_api import get_github_user_info
 
 app = Flask(__name__)
 
@@ -6,6 +7,25 @@ app = Flask(__name__)
 def homepage():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    author = 'wyatt'
+    class_code = 2905
+    concepts = ['flask', 'web apps', 'html']
+    return render_template('about.html', author=author, class_code=class_code, concepts=concepts)
+
+@app.route('/get_user')
+def get_github_user():
+    print(request.args)
+    username = request.args.get('username')
+    
+    user_info = get_github_user_info(username)
+    # todo make API request
+    # todo send data about user in response
+    if username:
+        return render_template('github.html', username=username, user_info=user_info)
+    else:
+        return 'error'
 
 if __name__ == '__main__':
     app.run()
--- github_api.py
+++ github_api.py
@@ -1,1 +1,8 @@
 # TODO make requests to github API
+
+import requests
+
+def get_github_user_info(username):
+    response = requests.get(f'https://api.github.com/users/{username}').json()
+    print(response)
+    return response
